e4102cf8d5f5c288971a0009df196f4e4daa5739
Adam Moore <adamaoc@gmail.com>
2016-04-12 19:29:08 -0500

    update buttons with large and xlarge

--- examples/components/buttons.php
+++ examples/components/buttons.php
@@ -25,5 +25,21 @@
       <button class="btn-pri--inverse">Primary Button</button>
       <button class="btn-base--inverse">Primary Alt Btn</button>
     </div>
+    <div class="btn-group" style="margin: 1em;">
+      <label style="width: 150px; display: inline-block;">Base Buttons</label>
+      <button class="btn-pri btn--large">Large Button</button>
+      <button class="btn-sec btn--large">Large Button</button>
+      <button class="btn-pri-alt btn--large">Large Button</button>
+      <br>
+      <br>
+      <label style="width: 150px; display: inline-block;">Large Buttons</label>
+      <button class="btn-sec-alt btn--large">Large Button</button>
+      <button class="btn-pri--outline btn--large">Large Button</button>
+      <button class="btn-sec--outline btn--large">Large Button</button>
+      <br>
+      <br>
+      <label style="width: 150px; display: inline-block;">X-Large Buttons</label>
+      <button class="btn-pri btn--xlarge">X-Large Button</button>
+    </div>
   </div>
 </div>
