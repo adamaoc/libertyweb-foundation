34be6f774ed457c34e704bb0acd538756d12f6c0
Adam Moore <adamaoc@gmail.com>
2016-04-14 05:53:58 -0500

    adding buttons

--- examples/components/buttons.php
+++ examples/components/buttons.php
@@ -1,44 +1,44 @@
 <div class="demo-wrapper" style="margin: 1em auto;">
   <div class="grid-pad">
     <div class="btn-group" style="margin: 1em;">
-      <label style="width: 150px; display: inline-block;">Base Buttons</label>
+      <label style="width: 180px; display: inline-block;">Base Buttons</label>
       <button>Base Button</button>
       <button class="btn-base-alt">Base Button</button>
       <button class="btn-base--outline">Base Button</button>
     </div>
     <div class="btn-group" style="margin: 1em;">
-      <label style="width: 150px; display: inline-block;">Primary Buttons</label>
+      <label style="width: 180px; display: inline-block;">Primary Buttons</label>
       <button class="btn-pri">Primary Button</button>
       <button class="btn-pri-alt">Primary Alt Btn</button>
       <button class="btn-pri--outline">Primary Button</button>
     </div>
     <div class="btn-group" style="margin: 1em;">
-      <label style="width: 150px; display: inline-block;">Secondary Buttons</label>
+      <label style="width: 180px; display: inline-block;">Secondary Buttons</label>
       <button class="btn-sec">Primary Button</button>
       <button class="btn-sec-alt">Primary Alt Btn</button>
       <button class="btn-sec--outline">Primary Alt Btn</button>
     </div>
     <div class="btn-group" style="margin: 1em; padding: 1em; background: #444;">
-      <label style="width: 150px; display: inline-block;color: white;">
+      <label style="width: 180px; display: inline-block;color: white;">
         Secondary Buttons
       </label>
       <button class="btn-pri--inverse">Primary Button</button>
       <button class="btn-base--inverse">Primary Alt Btn</button>
     </div>
     <div class="btn-group" style="margin: 1em;">
-      <label style="width: 150px; display: inline-block;">Base Buttons</label>
+      <label style="width: 180px; display: inline-block;">Base Buttons</label>
       <button class="btn-pri btn--large">Large Button</button>
       <button class="btn-sec btn--large">Large Button</button>
       <button class="btn-pri-alt btn--large">Large Button</button>
       <br>
       <br>
-      <label style="width: 150px; display: inline-block;">Large Buttons</label>
+      <label style="width: 180px; display: inline-block;">Large Buttons</label>
       <button class="btn-sec-alt btn--large">Large Button</button>
       <button class="btn-pri--outline btn--large">Large Button</button>
       <button class="btn-sec--outline btn--large">Large Button</button>
       <br>
       <br>
-      <label style="width: 150px; display: inline-block;">X-Large Buttons</label>
+      <label style="width: 180px; display: inline-block;">X-Large Buttons</label>
       <button class="btn-pri btn--xlarge">X-Large Button</button>
     </div>
   </div>
